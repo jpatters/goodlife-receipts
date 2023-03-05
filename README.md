54b2bef23ce03a5ebba56418c927554b38df7b57
Jordan Patterson <jordan@jpatterson.me>
2023-03-05 07:43:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Fetch receipts from Goodlife
+
+Goodlife doesn't send receipts automatically (possibly illegal?). The company I work for provides a yearly wellness bonus so I need my receipts to submit for reimbursement. I have better things to do than try to remember to go fill out this long form on their site every two weeks. So I made this.
+
+They don't provide access to receipts until 7 days after the charge, so note the `startDate` and `endDate` parameters.
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	uri := "https://receipts.goodlifefitness.com/"
+	startDate := time.Now().AddDate(0, 0, -14).Format("01/02/2006")
+	endDate := time.Now().AddDate(0, 0, -7).Format("01/02/2006")
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	parsedValues := getPageValues(resp.Body, []string{"__VIEWSTATE", "__EVENTVALIDATION", "__VIEWSTATEGENERATOR"})
+
+	for key, value := range parsedValues {
+		if value == "" {
+			panic(fmt.Sprintf("Could not find %s", key))
+		}
+	}
+
+	log.Println("Got values from page")
+
+	form := url.Values{}
+
+	for key, value := range parsedValues {
+		form.Add(key, value)
+	}
+
+	form.Add("__EVENTTARGET", "ctl00$Copy$btnSubmit3day")
+	form.Add("ctl00$ScriptManager1", "")
+	form.Add("ctl00$Copy$first_name", os.Getenv("FIRST_NAME"))
+	form.Add("ctl00$Copy$last_name", os.Getenv("LAST_NAME"))
+	form.Add("ctl00$Copy$revcan_number", os.Getenv("BARCODE_NUMBER"))
+	form.Add("ctl00$Copy$member_number", "")
+	form.Add("ctl00$Copy$drpBirthMonth", os.Getenv("BIRTH_MONTH"))
+	form.Add("ctl00$Copy$drpBirthDay", os.Getenv("BIRTH_DAY"))
+	form.Add("ctl00$Copy$drpBirthYear", os.Getenv("BIRTH_YEAR"))
+	form.Add("ctl00$Copy$PayorFirstName", "")
+	form.Add("ctl00$Copy$PayorLastName", "")
+	form.Add("ctl00$Copy$ParentBarcode", "")
+	form.Add("ctl00$Copy$ParentMembershipNumber", "")
+	form.Add("ctl00$Copy$street", os.Getenv("STREET_ADDRESS"))
+	form.Add("ctl00$Copy$city", os.Getenv("CITY"))
+	form.Add("ctl00$Copy$province", os.Getenv("PROVINCE"))
+	form.Add("ctl00$Copy$postal_code", os.Getenv("POSTAL_CODE"))
+	form.Add("ctl00$Copy$email", os.Getenv("EMAIL_ADDRESS"))
+	form.Add("ctl00$Copy$email2", os.Getenv("EMAIL_ADDRESS"))
+	form.Add("ctl00$Copy$telephone", os.Getenv("PHONE_NUMBER"))
+	form.Add("ctl00$Copy$startdate", startDate)
+	form.Add("ctl00$Copy$enddate", endDate)
+	form.Add("ctl00$Copy$CheckBox1", "on")
+
+	log.Println("Sending request")
+	response, err := http.Post(uri, "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		panic(fmt.Sprintf("Status code error: %d %s", response.StatusCode, response.Status))
+	}
+
+	log.Println("Success")
+}
+
+func getPageValues(body io.Reader, searchValues []string) map[string]string {
+	doc, err := goquery.NewDocumentFromReader(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := make(map[string]string)
+
+	for _, searchValue := range searchValues {
+		results[searchValue] = doc.Find(fmt.Sprintf("#%s", searchValue)).AttrOr("value", "")
+	}
+
+	return results
+}
